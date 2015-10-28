2f76f63793047ebdd280d681342939647f7f9691
RatneshThakur <saasbook@saasbook.(none)>
2015-10-27 20:02:36 -0500

    adding graph view page controller and view

---
+++ app/controllers/graphs_controller.rb
@@ -0,0 +1,7 @@
+class GraphsController < ApplicationController
+  def index
+  	puts "Hello world"
+  end
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  
+  resources :graphs
+
+
  # resources :uploads/new
   
  # get 'uploads/new'
